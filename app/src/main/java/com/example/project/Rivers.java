b8ffa282f6c86eeaf39110ba198ee18601f42666
b21oscpe <b21oscpe@student.his.se>
2022-05-23 21:26:30 +0200

    wrote code for my Rivers.java class

--- app/src/main/java/com/example/project/Rivers.java
+++ app/src/main/java/com/example/project/Rivers.java
@@ -2,5 +2,57 @@ package com.example.project;
 
 public class Rivers {
 
+    String ID;
+    String name;
+    String location;
+    Integer size;
+    String auxdata;
 
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public String getAuxdata() {
+        return auxdata;
+    }
+
+    public void setAuxdata(String auxdata) {
+        this.auxdata = auxdata;
+    }
+
+    public Rivers(String ID, String name, String location, Integer size, String auxdata){
+        this.ID = ID;
+        this.name = name;
+        this.location = location;
+        this.size = size;
+        this.auxdata = auxdata;
+    }
 }
