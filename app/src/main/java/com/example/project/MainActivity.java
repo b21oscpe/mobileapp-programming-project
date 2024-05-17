34aafded2c43f0aabe5f0180b7fa98868b87f238
Oscar <b21oscpe@student.his.se>
2024-05-17 16:59:05 +0200

    Added a new activity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.project;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -20,11 +24,24 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     private ArrayList<River> rivers;
     private RecyclerView recyclerView;
 
+    private Button about_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new JsonTask(this).execute(JSON_URL);
+
+        about_button = findViewById(R.id.about_button);
+        about_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Log.d("==>", "Navigated to 'About'");
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @SuppressLint("NotifyDataSetChanged")
