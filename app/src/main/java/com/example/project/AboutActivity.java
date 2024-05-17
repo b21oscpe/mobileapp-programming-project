00996b28ee4696ac6e07ec0b88a1904eabd77e3f
Oscar <b21oscpe@student.his.se>
2024-05-17 17:40:49 +0200

    added webview for second activity

--- app/src/main/java/com/example/project/AboutActivity.java
+++ app/src/main/java/com/example/project/AboutActivity.java
@@ -3,13 +3,22 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class AboutActivity extends AppCompatActivity {
+
+    WebView myWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        myWebView=findViewById(R.id.my_webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("file:///android_asset/about.html");
     }
 }
